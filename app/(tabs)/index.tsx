504b5f8d32f8961543d8a547a06e8ac83d29abdc
Mukesh Gouda <109750504+itmukesh-16@users.noreply.github.com>
2026-04-07 11:04:38 +0530

    presseble button component added and primary feature is applied

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
 import { StyleSheet, View } from "react-native";
 
-import ImageViewer from "@/components/imageViewer";
+import Button from "@/components/button"; // this include button component
+import ImageViewer from "@/components/imageViewer"; // it includes imageViewer
 
-const PlaceholderImage = require("@/assets/images/background-image.png");
+const PlaceholderImage = require("@/assets/images/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
+        {/* img component */}
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      {/* this section is button section  */}
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -22,5 +29,9 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
--- components/imageViewer.tsx
+++ components/imageViewer.tsx
@@ -1,10 +1,13 @@
 import { Image } from "expo-image";
 import { ImageSourcePropType, StyleSheet } from "react-native";
 
+// it tells what this imageViewer component expects as a ipun
+// ImageSourcePropType tells it will take a local path or a URL path for images
 type Props = {
   imgSource: ImageSourcePropType;
 };
 
+// ImageViewer function accepts destructures the Props into image and props
 export default function ImageViewer({ imgSource }: Props) {
   return <Image source={imgSource} style={styles.image} />;
 }
